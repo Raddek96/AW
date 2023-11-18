0eb3db680de17cbbb847e182dba06d720da28220
Radoslaw Lisiecki <rlisiecki06@gmail.com>
2023-11-18 16:19:47 +0100

    Actualizando scriptDefinitivo

--- Apuntes SOR/scripts/scriptDefinitivo.sh
+++ Apuntes SOR/scripts/scriptDefinitivo.sh
@@ -30,7 +30,8 @@ echo "Introduce tu dominio en minúsculas y sin extensión como por ejemplo, ies
 read DOMINIOMIN
 
 #*Se almacena en la variable MONTAJE la ruta sobre la que vas a montar el RAID*
-read -p "Introduce la ruta de tu directorio sobre el que montarás el RAID, ejemplo /datos" MONTAJE
+echo "Introduce la ruta de tu directorio sobre el que montarás el RAID, ejemplo /datos" 
+read MONTAJE
 
 #*Se almacena en la variable DOMINIOMAS el valor de $DOMINIOMIN EN MAYÚSCULAS GRACIAS al COMANDO tr que permite la transliteración de caracteres de minúscula a mayúscula.*
 DOMINIOMAS=$(echo "$DOMINIOMIN" | tr '[:lower:]' '[:upper:]')
@@ -82,6 +83,12 @@ sed -i "s/.*/domain $DOMINIOMIN.local\nsearch $DOMINIOMIN.local\nnameserver $IPS
 sleep 2
 
 #Configuración del archivo hosts
+#*sed -i nos permite modificar directamente el archivo en vez de redirigir la salida a otro archivo*
+#*la s indica que se va a realizar una operación de sustitución*
+#*/ indica el comienzo del primer patrón, \( sirve para interpretar el paréntesis como un caracter al igual de \.*
+#*[[:space:]]*\ esto indica que hay espacios en blanco, el * indica que no se especifica cuantos espacios en blanco hay*
+#*\).*, interpreta el paréntesis como caracter y el .* indica que puede haber cualquier cosa después*
+#*\1 se refiere al primer patrón *
 sed -i "s/\(127\.0\.1\.1[[:space:]]*\).*/\1$HOSTNAME $HOSTNAME.$DOMINIOMIN.local/" /etc/hosts
 sleep 2
 
@@ -113,7 +120,7 @@ host -t SRV _ldap._tcp.$DOMINIOMIN.local
 host -t SRV _kerberos._udp.$DOMINIOMIN.local
 host -t A $HOSTNAME.$DOMINIOMIN.local
 
-read -p "Presiona Enter para continuar con la parte de importar usuarios"
+read -p "Presiona Enter para continuar con la parte de importar usuarios, presiona ENTER para continuar con la importación de los usuarios"
 
 #Instalación y configuración del servicio nslcd (usar opciones por defecto y marcar passwd, group y shadow)
 apt install nslcd -y
@@ -142,11 +149,15 @@ systemctl restart nslcd
 
 #Creación de usuario de prueba
 samba-tool user add usuarioPrueba abc123. --uid-number=10022
+samba-tool group addunixattrs "Domain Admins" 20010
+
+
+
 sleep 2
 
 getent passwd | grep usuarioPrueba
 
-read -p "Usuarios y grupos importados correctamente"
+read -p "Usuarios y grupos importados correctamente, presiona ENTER para continuar"
 
 samba-tool user delete usuarioPrueba
 
@@ -155,7 +166,7 @@ samba-tool user delete usuarioPrueba
 echo "Instalación de mdadm, acl, y quota."
 apt install mdadm acl quota -y
 
-echo "Script finalizado"
+echo "Creación del RAID"
 sleep 2
 
 
@@ -164,13 +175,85 @@ sleep 2
 ##CREACIÓN DEL RAID
 
 
+#Creación del raid
+yes | mdadm --create /dev/md0 --level=1 --raid-devices=2 /dev/sdb /dev/sdc
+sleep 10
 
-mdadm --create /dev/md0 --level=1 --raid-devices=2 /dev/sdb /dev/sdc
-sleep 10 
+#Creación del directorio y subdirectorios
+mkdir $MONTAJE
 mdadm --detail --scan >> /etc/mdadm/mdadm.conf
 yes | mkfs.ext4 /dev/md0
-update-initframs -u
+update-initramfs -u
 echo "/dev/md0 $MONTAJE ext4 user_xattr,acl,usrquota,grpquota 0 2" >> /etc/fstab
 systemctl daemon-reload
 mount -a
 mount | grep md0
+mkdir $MONTAJE/diradmin $MONTAJE/diraux $MONTAJE/PerfisWindows $MONTAJE/PerfisLinux
+chown -R root:root $MONTAJE
+chmod -R 770 $MONTAJE
+chmod 775 $MONTAJE
+
+#Compartir recurso de nombre users.
+#-e para que interprete \ para escapar caracteres
+echo -e "[users]\n\tpath = $MONTAJE\n\tread only = No" >> /etc/samba/smb.conf
+
+
+#Creación de usuarios, grupos y unidades organizativas (adaptado para el ejercicio de práctica)
+#Creación de las unidades organizativas (En este caso uoadm y uoaux estarán dentro de uouser)
+samba-tool ou create "OU=uouser,DC=iesallerrl,DC=local"
+samba-tool ou create "OU=uoadm,OU=uouser,DC=iesallerrl,DC=local"
+samba-tool ou create "OU=uoaux,OU=uouser,DC=iesallerrl,DC=local"
+
+
+#Creación de los grupos y ubicarlos en su uo correspondiente
+samba-tool group add g-user --groupou=OU=uouser --gid-number=20000 --nis-domain=iesallerrl.local
+samba-tool group add g-adm --groupou=OU=uoadm,OU=uouser --gid-number=20001 --nis-domain=iesallerrl.local
+samba-tool group add g-aux --groupou=OU=uoaux,OU=uouser --gid-number=20002 --nis-domain=iesallerrl.local
+
+#Creación de los usuarios y ubicarlos en su uo correspondiente
+samba-tool domain passwordsettings set --max-pwd-age=0
+samba-tool user create uadm1 abc123. --userou=OU=uoadm,OU=uouser --uid-number=10000 --profilepath="\\$HOSTNAME\users\diradmin\%username%" --homedrive=P: --homepath="\\$HOSTNAME\users\PerfisWindows\%username"
+samba-tool user create uaux1 abc123. --userou=OU=uoaux,OU=uouser --uid-number=10001 --profilepath="\\$HOSTNAME\users\diraux\%username%" --homedrive=P: --homepath="\\$HOSTNAME\users\PerfisWindows\%username" 
+
+
+
+#Añadir los usuarios a sus grupos correspondientes
+samba-tool group addmembers g-user uadm1 
+samba-tool group addmembers g-user uaux1
+samba-tool group addmembers g-adm uadm1 
+samba-tool group addmembers g-aux  uaux1
+
+
+#ACL
+
+#Directorio padre
+setfacl -m g:g-user:r-x $MONTAJE
+setfacl -m g:"Domain Admins":rwx $MONTAJE
+setfacl -dm g:"Domain Admins":rwx $MONTAJE
+
+#Directorio diradmin
+setfacl -m g:"Domain Admins":rwx $MONTAJE/diradmin
+setfacl -m g:g-adm:r-x $MONTAJE/diradmin
+setfacl -dm g::--- $MONTAJE/diradmin
+setfacl -dm o:--- $MONTAJE/diradmin
+
+#Directorio diraux
+setfacl -m g:"Domain Admins":rwx $MONTAJE/diraux
+setfacl -m g:g-aux:r-x $MONTAJE/diraux
+setfacl -dm g::--- $MONTAJE/diraux
+setfacl -dm o:--- $MONTAJE/diraux
+
+#Directorio PerfisWindows
+setfacl -m g:g-user:rwx $MONTAJE/PerfisWindows
+
+#Directorio PerfisLinux
+setfacl -m g:g-user:rwx $MONTAJE/PerfisLinux
+
+#Quotas
+
+quotaoff -avug
+quotacheck -avug
+quotaon -avug
+
+setquota -u uadm1 150000 300000 0 0 /dev/md0
+setquota -u uaux1 75000 150000 0 0 /dev/md0
