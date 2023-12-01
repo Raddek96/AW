73b40f1f98481d6969b31329ac520920817e2a17
Radek <141306936+Raddek96@users.noreply.github.com>
2023-12-01 13:22:33 +0000

    Javascript reorganizado :D

--- Proyectos/Proyecto AW/main.js
+++ Proyectos/Proyecto AW/main.js
@@ -1,164 +1,186 @@
+// Autor: Radoslaw Lisiecki
+// Fecha: 01-12-2023
+// Versión: 1.0
+
+
+
+// Variables y elementos del DOM
+const FLEXCONTAINER = document.getElementById("flexContainer");
+const alignContent = document.getElementById("align-content");
+const WIDTH = document.getElementById("width");
+const HEIGHT = document.getElementById("height");
+const CAJAS = document.getElementsByClassName("flexChilds");
+const MINUS = document.getElementById("minus");
+const PLUS = document.getElementById("plus");
+let cajasVisibles = 3;
+const selectFlexDirection = document.getElementById("flex-direction");
+const selectFlexWrap = document.getElementById("flex-wrap");
+const selectJustifyContent = document.getElementById("justify-content");
+const alignItems = document.getElementById("align-items");
+const arrayCajas = Array.from(CAJAS);
+const buttonReset = document.getElementById("reset");
+const witdthBox = document.getElementById("widthBox");
+const heightBox = document.getElementById("heightBox");
+
+
+
+//Funciones
+
+// Genera un código con formato rgb aleatorio
 function generarColorAleatorio() {
   // Generar valores aleatorios para los componentes R, G, y B
   const componenteR = Math.floor(Math.random() * 256); // Valores entre 0 y 255
   const componenteG = Math.floor(Math.random() * 256);
   const componenteB = Math.floor(Math.random() * 256);
 
-// Después
-const colorRGB = `rgb(${componenteR}, ${componenteG}, ${componenteB})`;
-return colorRGB;
+  // Almacena el color en formato RGB
+  const colorRGB = `rgb(${componenteR}, ${componenteG}, ${componenteB})`;
+  return colorRGB;
 }
 
 
-const FLEXCONTAINER = document.getElementById("flexContainer");
-const alignContent = document.getElementById("align-content");
+//Modifica el tamaño del contenedor padre o de los contenedores hijos
+function modificarTamaño(valor, size, boolean){
+  if (!isNaN(valor)) {
+     var valorValue = parseFloat(valor);
+     if(boolean === "true"){
 
-const WIDTH = document.getElementById("width");
-const HEIGHT = document.getElementById("height");
-
-WIDTH.addEventListener("input", () => {
-if (!isNaN(WIDTH.value)) {
-  widthValue = WIDTH.value;
-  console.log(widthValue);
-  FLEXCONTAINER.style.width = widthValue + "px";
-  WIDTH.placeholder = "";
+    if(size === "width") {
+      FLEXCONTAINER.style.width = valorValue + "px";
+      WIDTH.placeholder = "";
+  } else if (size === "height") {
+    FLEXCONTAINER.style.height = valorValue + "px";
+    HEIGHT.placeholder = "";
+  }
 } else {
-  console.error("Se ha introducido un valor no numérico");
+  arrayCajas.forEach(function(caja) {
+    if(size === "width") {
+      caja.style.width = valorValue + "px";
+      witdthBox.placeholder = "";
+  } else if (size === "height") {
+    caja.style.height = valorValue + "px";
+    heightBox.placeholder = "";
+  }
+  });
+
 }
-});
-
-HEIGHT.addEventListener("input", () => {
-if (!isNaN(HEIGHT.value)) {
-  heightValue = HEIGHT.value;
-  console.log(widthValue);
-  FLEXCONTAINER.style.height = heightValue + "px";
-  HEIGHT.placeholder = "";
-} else {
-  console.error("Se ha introducido un valor no numérico");
-}
-});
-
-const CAJAS = document.getElementsByClassName("flexChilds");
-
-
-const MINUS = document.getElementById("minus");
-const PLUS = document.getElementById("plus");
-let cajasVisibles = 3;
-
-PLUS.addEventListener("click", () => {
-if (cajasVisibles < CAJAS.length) {
-  CAJAS[cajasVisibles].style.display = "block";
-  cajasVisibles++;
-  if (cajasVisibles > 0) {
-    MINUS.style.background = "red";
-    MINUS.style.cursor = "pointer";
-    if (cajasVisibles === CAJAS.length) {
-      PLUS.style.background = "rgb(128, 128, 128)";
-      PLUS.style.cursor = "default";
+    } else {
+      console.error("Se ha introducido un valor no numérico");
     }
+    
+}
+
+//Cambiar propiedades flex del contenedor padre
+function cambiarPropiedadFlex(propiedad, valor) {
+  FLEXCONTAINER.style[propiedad] = valor;
+}
+
+//Habilitar o deshabilitar AlignContent
+function boolAlignContent(){
+  if (selectFlexWrap.value === "wrap") {
+    alignContent.disabled = false;
+  } else {
+    alignContent.disabled = true;
   }
 }
+
+
+// Event Listeners
+//Anchura del contenedor padre
+WIDTH.addEventListener("input", () => {
+  modificarTamaño(WIDTH.value, "width", "true")
 });
 
-MINUS.addEventListener("click", () => {
-if (cajasVisibles > 0) {
-  cajasVisibles--;
-  CAJAS[cajasVisibles].style.display = "none";
-  if (cajasVisibles === 0) {
-    MINUS.style.background = "rgb(128, 128, 128)";
-    MINUS.style.cursor = "default";
-  }
-  if (cajasVisibles < CAJAS.length) {
-    PLUS.style.background = "green";
-    PLUS.style.cursor = "pointer";
-  }
-}
+//Altura del contenedor padre
+HEIGHT.addEventListener("input", () => {
+  modificarTamaño(HEIGHT.value, "height", "true")
 });
 
-const selectFlexDirection = document.getElementById("flex-direction");
+//Anchura de los contenedores hijos
+witdthBox.addEventListener("input", () => {
+  modificarTamaño(witdthBox.value, "width", "false");
+});
 
+//Altura de los contenedor hijos
+heightBox.addEventListener("input", () => {
+  modificarTamaño(heightBox.value, "height", "false");
+});
+
+//Cambia el estilo de flexDirection
 selectFlexDirection.addEventListener("change", () => {
-FLEXCONTAINER.style.flexDirection = selectFlexDirection.value;
+  cambiarPropiedadFlex("flexDirection", selectFlexDirection.value);
 });
 
-const selectFlexWrap = document.getElementById("flex-wrap");
-
-selectFlexWrap.addEventListener("change", () => {
-FLEXCONTAINER.style.flexWrap = selectFlexWrap.value;
-if(selectFlexWrap.value === "wrap"){
-  alignContent.disabled = false;
-} else {
-  alignContent.disabled = true;
-}
-});
-
-const selectJustifyContent = document.getElementById("justify-content");
-
+//Cambia el estilo de justifyContent
 selectJustifyContent.addEventListener("change", () => {
-FLEXCONTAINER.style.justifyContent = selectJustifyContent.value;
+  cambiarPropiedadFlex("justifyContent", selectJustifyContent.value);
 });
 
-const alignItems = document.getElementById("align-items");
-
+//Cambia el estilo de alignItems
 alignItems.addEventListener("change", () => {
-FLEXCONTAINER.style.alignItems = alignItems.value;
+  cambiarPropiedadFlex("alignItems", alignItems.value);
 });
 
-
-
+//Cambia el estilo de alignContent
 alignContent.addEventListener("change", () => {
-  FLEXCONTAINER.style.alignContent = alignContent.value;
+  cambiarPropiedadFlex("alignContent", alignContent.value);
 });
 
-const arrayCajas = Array.from(CAJAS);
+//Cambia el estilo de flexWrap y habilita o deshabilita alignContent
+selectFlexWrap.addEventListener("change", () => {
+  cambiarPropiedadFlex("flexWrap", selectFlexWrap.value);
+  boolAlignContent();
+});
+
+//Cambia el color de cada contenedor hijo a un colo aleatorio
 arrayCajas.forEach(caja => {
   caja.addEventListener("click", () => {
-      caja.style.background = generarColorAleatorio();
+    caja.style.background = generarColorAleatorio();
   })
 });
 
-const buttonReset = document.getElementById("reset");
+//Configura el funcionamiento del botón de añadir (display) de los contenedores hijos
+PLUS.addEventListener("click", () => {
+  if (cajasVisibles < CAJAS.length) {
+    CAJAS[cajasVisibles].style.display = "block";
+    cajasVisibles++;
+    if (cajasVisibles > 0) {
+      MINUS.style.background = "red";
+      MINUS.style.cursor = "pointer";
+      if (cajasVisibles === CAJAS.length) {
+        PLUS.style.background = "rgb(128, 128, 128)";
+        PLUS.style.cursor = "default";
+      }
+    }
+  }
+});
+
+//Configura el funcionamiento del botçon de quitar (display) de los contenedores hijos
+MINUS.addEventListener("click", () => {
+  if (cajasVisibles > 0) {
+    cajasVisibles--;
+    CAJAS[cajasVisibles].style.display = "none";
+    if (cajasVisibles === 0) {
+      MINUS.style.background = "rgb(128, 128, 128)";
+      MINUS.style.cursor = "default";
+    }
+    if (cajasVisibles < CAJAS.length) {
+      PLUS.style.background = "green";
+      PLUS.style.cursor = "pointer";
+    }
+  }
+});
+
+//Configura el botón de resetear los inputs y los selects
 buttonReset.addEventListener("click", () => {
-let inputs = window.document.querySelectorAll("input");
-let selects = window.document.querySelectorAll("select");
-inputs.forEach(function(input){
-  input.value = "";
-})
-
-selects.forEach(function(select){
-  select.selectedIndex = -1;
-})
-window.location.reload();
-
-});
-
-
-
-const witdthBox = document.getElementById("widthBox");
-const heightBox = document.getElementById("heightBox");
-
-witdthBox.addEventListener("input", () => {
-if (!isNaN(witdthBox.value)) {
-  widthValue = witdthBox.value;
-  console.log(widthValue);
-  arrayCajas.forEach(function(caja) {
-    caja.style.width = widthValue + "px";
+  let inputs = window.document.querySelectorAll("input");
+  let selects = window.document.querySelectorAll("select");
+  inputs.forEach(function (input) {
+    input.value = "";
   });
-  witdthBox.placeholder = "";
-} else {
-  console.error("Se ha introducido un valor no numérico");
-}
-});
 
-heightBox.addEventListener("input", () => {
-if (!isNaN(heightBox.value)) {
-  heightValue = heightBox.value;
-  console.log(heightValue);
-  arrayCajas.forEach(function(caja) {
-    caja.style.height = heightValue + "px";
+  selects.forEach(function (select) {
+    select.selectedIndex = -1;
   });
-  heightBox.placeholder = "";
-} else {
-  console.error("Se ha introducido un valor no numérico");
-}
+  window.location.reload();
 });
