1495265a7243e59804fbc8a4ecd5ada83bf9caa3
Radek <141306936+Raddek96@users.noreply.github.com>
2023-12-14 17:18:13 +0000

    pizza

--- Proyectos/Pizzeria/main.js
+++ Proyectos/Pizzeria/main.js
@@ -1,5 +1,6 @@
 const pizzas = Array.from(document.getElementsByClassName("pizzas"));
 const image = document.getElementById("image");
+const tituloPizza = document.getElementById("tituloPizza");
 
 pizzas.forEach((pizza, index) => {
     pizza.addEventListener("click", () => {
@@ -14,19 +15,24 @@ pizzas.forEach((pizza, index) => {
 
         switch(index){
             case 0:
-                image.src = "img/pizzas/4Quesos.png"
+                image.src = "img/pizzas/4Quesos.png";
+                tituloPizza.textContent = "4 QUESOS";
             break;
             case 1:
                 image.src = "img/pizzas/Barbacoa.png"
+                tituloPizza.textContent = "BARBACOA";
             break;
             case 2:
                 image.src = "img/pizzas/hawaiana.png"
+                tituloPizza.textContent = "HAWAIANA";
             break;
             case 3:
                 image.src = "img/pizzas/Margarita.png"
+                tituloPizza.textContent = "MARGARITA";
             break;
             case 4:
                 image.src = "img/pizzas/Vegetariana.png"
+                tituloPizza.textContent = "VEGETARIANA";
             break;
             
         }
